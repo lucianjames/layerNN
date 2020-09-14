f2bcb81ada37f86c91dc3772bcbe56d8e1b1cafb
lucianojamimbo <65134690+lucianojamimbo@users.noreply.github.com>
2020-09-14 12:28:46 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include "vectio.h"
+#include "math.h"
+#include "mnist.h"
+#include "activfuncs.h"
+#include "lossfuncs.h"
+using namespace std;
+
+
+int main(){
+
+  return 0;
+}
