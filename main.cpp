31424a2198a2aef00cf723ebe8034986e75df1b1
lucianojamimbo <65134690+lucianojamimbo@users.noreply.github.com>
2020-09-14 15:50:21 +0100

    simpleGD working on sigmoidlayer
    next task is stochasticGD

--- main.cpp
+++ main.cpp
@@ -130,10 +130,11 @@ class sigmoidlayer : public baselayer{
 private:
 
 public:
+  vector<float> sigp; //sigmoid prime
   sigmoidlayer(const float& pls, //previous layer size
                const float& ls //layer size
                ) : baselayer(pls, ls){
-      // Add any layer-specific construction code here, if necessary
+      this->sigp.resize(ls);
   }
 
   //feedforwards with sigmoid activation function
@@ -144,11 +145,45 @@ public:
     }
     vectsigmoid(this->pfa, this->a);
   }
+
+  //backprop for sigmoid neurons:
+  void backprop(const vector<float>& nd, //Next layer Delta
+                const vector<vector<float>>& ntpw //Next layer TransPosed Weights
+                ){
+    //set own tpw so previous layer can backprop
+    transpose(this->w, this->tpw);
+    //calculate delta
+    for (int neuron = 0; neuron < this->d.size(); neuron++){ //get dotprod of tpw and delta[layer+1]
+        dot(ntpw[neuron], nd, this->d[neuron]);
+      }
+    vectsigmoidprime(this->pfa, this->sigp );
+    hadamard(this->d, this->sigp , this->d);
+  }
+};
+class sigmoidoutputlayer : public sigmoidlayer{
+private:
+
+public:
+  sigmoidoutputlayer(const float& pls, //previous layer size
+                     const float& ls //layer size
+                    ) : sigmoidlayer(pls, ls){
+      // Add any layer-specific construction code here, if necessary
+  }
+
+  //backprop for sigmoid output neurons:
+  void backprop(const vector<float>& desiredoutput){
+    //set own tpw so previous layer can backprop
+    transpose(this->w, this->tpw);
+    //calculate delta
+    MSEderivative(this->a, desiredoutput, this->d);
+    vectsigmoidprime(this->pfa, this->sigp );
+    hadamard(this->d, this->sigp , this->d);
+  }
 };
 
 int main(){
-  baselayer l1(2, 2);
-  baseoutputlayer l2(2, 2);
+  sigmoidlayer l1(2, 2);
+  sigmoidoutputlayer l2(2, 2);
 
   l1.paraminit(-1, 1);
   l2.paraminit(-1, 1);
@@ -173,8 +208,8 @@ int main(){
       vectbyscalarmultiply(l1.a, l1.d[n], l1.nw[n]);
     }
     //update parameters of all layers
-    l1.updateparams(0.1);
-    l2.updateparams(0.1);
+    l1.updateparams(1);
+    l2.updateparams(1);
     //print out activations to terminal:
     cout << "l2 activations:" << endl;
     printV(l2.a);
