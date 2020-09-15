2030ccbd7a8149253be77852cc525fafcdd0447e
lucianojamimbo <65134690+lucianojamimbo@users.noreply.github.com>
2020-09-15 17:52:02 +0100

    95% accuracy simpleGD on mnist dataset

--- main.cpp
+++ main.cpp
@@ -35,18 +35,17 @@ int main(){
 
   //network hyperparameters:
   float eta = 1;
+  int n_epochs = 10;
+  //dataset info:
+  int n_imgs = 60000;
+  int n_timgs = 10000;
+
 
   //create vector of pointers to class objects to define the network:
   vector<baselayer*> lyrs;
-  sigmoidlayer* l1 = new sigmoidlayer(2,5);
-  sigmoidlayer* l2 = new sigmoidlayer(5,2);
-  sigmoidlayer* l3 = new sigmoidlayer(2,10);
-  sigmoidlayer* l4 = new sigmoidlayer(10,2);
-  sigmoidoutputlayer* outputlayer = new sigmoidoutputlayer(2,2);
+  sigmoidlayer* l1 = new sigmoidlayer(784,32);
+  sigmoidoutputlayer* outputlayer = new sigmoidoutputlayer(32,10);
   lyrs.push_back (l1);
-  lyrs.push_back (l2);
-  lyrs.push_back (l3);
-  lyrs.push_back (l4);
   lyrs.push_back (outputlayer);
 
   //init network params:
@@ -54,34 +53,58 @@ int main(){
     l->paraminit(-1, 1);
   }
 
-  //dummy gradient descent test:
-  vector<float> inp = {0.8, 0.9};
-  vector<float> desiredout = {0, 0};
+  //MNIST simple gradient descent test:
+  vector<float> inp;
+  inp.resize(784);
+  vector<float> desiredout;
+  desiredout.resize(10);
+  for (int epoch = 0; epoch < n_epochs; epoch++){
+    for (int img = 0; img < n_imgs; img++){
+      inp = imgs[img];
+      for (auto& i : desiredout){
+        i = 0;
+      }
+      desiredout[labels[img]] = 1;
 
-  for (int loops = 0; loops < 100; loops++){
+      //run forwards pass:
+      l1->feedforwards(inp);
+      for (int l = 1; l < lyrs.size(); l++){
+        lyrs[l]->feedforwards(lyrs[l-1]->a);
+      }
+      //run backprop pass:
+      outputlayer->backprop(desiredout);
+      for (int l = lyrs.size()-2; l > -1; l--){
+        lyrs[l]->backprop(lyrs[l+1]->d, lyrs[l+1]->tpw);
+      }
+      //calc nablas for all layers:
+      l1->calcnablas(inp);
+      for (int l = 1; l < lyrs.size(); l++){
+        lyrs[l]->calcnablas(lyrs[l-1]->a);
+      }
+      //update all layer params:
+      for (auto& l : lyrs){
+        l->updateparams(eta);
+      }
+    }
+  }
+
+
+
+  //run a little test:
+  int correctclassifications = 0;
+  for (int testimg = 0; testimg < n_timgs; testimg++){
+    inp = imgs[testimg];
     //run forwards pass:
     l1->feedforwards(inp);
     for (int l = 1; l < lyrs.size(); l++){
       lyrs[l]->feedforwards(lyrs[l-1]->a);
     }
-    //run backprop pass:
-    outputlayer->backprop(desiredout);
-    for (int l = lyrs.size()-2; l > -1; l--){
-        lyrs[l]->backprop(lyrs[l+1]->d, lyrs[l+1]->tpw);
+    if ((max_element(outputlayer->a.begin(), outputlayer->a.end()) - outputlayer->a.begin()) == labels[testimg]){
+	     correctclassifications += 1;
       }
-    //calc nablas for all layers:
-    l1->calcnablas(inp);
-    for (int l = 1; l < lyrs.size(); l++){
-      lyrs[l]->calcnablas(lyrs[l-1]->a);
-    }
-    //update all layer params:
-    for (auto& l : lyrs){
-      l->updateparams(eta);
-    }
-    //print out network activation values:
-    printV(outputlayer->a);
   }
 
+  cout << correctclassifications << endl;
 
   return 0;
 }
